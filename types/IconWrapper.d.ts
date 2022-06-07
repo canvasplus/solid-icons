8a17ca4fdcf9ce2175f5e1a9a4fd31d8981d9b93
Adrian Casares <67245238+adriancasares@users.noreply.github.com>
2022-06-07 17:12:58 -0500

    remove intrinsic elements declaration

--- types/IconWrapper.d.ts
+++ types/IconWrapper.d.ts
@@ -1,37 +1,40 @@
 import type { JSXElement, JSX } from "solid-js/types";
-declare global {
-    namespace JSX {
-        interface IntrinsicElements {
-            svg: {
-                className?: string;
-                style?: object;
-                height?: number | string;
-                width?: number | string;
-                innerHTML: string;
-                xmlns: string;
-                strokeWidth?: string;
-                viewBox?: string;
-            };
-            title: any;
-        }
-    }
-}
+// declare global {
+//     namespace JSX {
+//         interface IntrinsicElements {
+//             svg: {
+//                 className?: string;
+//                 style?: object;
+//                 height?: number | string;
+//                 width?: number | string;
+//                 innerHTML: string;
+//                 xmlns: string;
+//                 strokeWidth?: string;
+//                 viewBox?: string;
+//             };
+//             title: any;
+//         }
+//     }
+// }
 export interface IconTree {
-    a: {
-        [key: string]: string;
-    };
-    c: string;
+  a: {
+    [key: string]: string;
+  };
+  c: string;
 }
 export interface IconProps extends JSX.SvgSVGAttributes<SVGElement> {
-    size?: string | number;
-    color?: string;
-    title?: string;
-    style?: object;
-    className?: string;
-    viewBox?: string;
+  size?: string | number;
+  color?: string;
+  title?: string;
+  style?: object;
+  className?: string;
+  viewBox?: string;
 }
 export interface IconBaseProps extends IconProps {
-    src: IconTree;
+  src: IconTree;
 }
 export declare type IconTypes = (props: IconProps) => JSXElement;
-export default function IconTemplate(iconSrc: IconTree, props: IconProps): JSXElement;
+export default function IconTemplate(
+  iconSrc: IconTree,
+  props: IconProps
+): JSXElement;
